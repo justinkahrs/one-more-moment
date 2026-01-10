f4a65f2bd16cf0211c7e0e26d01c651b19499ddb
Justin Kahrs <justin@justinkahrs.com>
2026-01-10 11:15:26 -0500

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [basicSsl(), tailwindcss()],
   },
+
   integrations: [mdx()],
+  adapter: vercel(),
 });
