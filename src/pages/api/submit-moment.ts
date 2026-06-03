4356042ecc6c12e22c84a97afd6a5ba95a1f1c02
spartski <jason@onemoremoment.org>
2026-06-03 10:27:51 -0400

    update wish request fields

--- src/pages/api/submit-moment.ts
+++ src/pages/api/submit-moment.ts
@@ -19,6 +19,41 @@ export const POST: APIRoute = async ({ request }) => {
 
   try {
     const data = await request.json();
+    const requesterName = data.requesterName || data.name || "";
+    const requesterEmail = data.requesterEmail || data.email || "";
+    const requesterPhone = data.requesterPhone || data.phone || "";
+    const warriorName = data.warriorName || "";
+    const relationshipToWarrior = data.relationshipToWarrior || "";
+    const situation = data.situation || "";
+    const situationDetails = [
+      warriorName ? `Person this Moment is for: ${warriorName}` : "",
+      relationshipToWarrior
+        ? `Requester relationship to this person: ${relationshipToWarrior}`
+        : "",
+      situation ? `Details: ${situation}` : "",
+    ]
+      .filter(Boolean)
+      .join("\n\n");
+    const subject = warriorName
+      ? `New Moment Request for ${warriorName}`
+      : "New One More Moment Wish Request";
+
+    const submission = {
+      ...data,
+      requestType: "Moment Request",
+      subject,
+      emailSubject: subject,
+      requesterName,
+      requesterEmail,
+      requesterPhone,
+      warriorName,
+      relationshipToWarrior,
+      rawSituation: situation,
+      situation: situationDetails || situation,
+      name: requesterName,
+      email: requesterEmail,
+      phone: requesterPhone,
+    };
 
     const response = await fetch(webhookUrl, {
       method: "POST",
@@ -26,7 +61,7 @@ export const POST: APIRoute = async ({ request }) => {
         "Content-Type": "application/json",
         "x-make-apikey": apiKey,
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify(submission),
     });
 
     if (!response.ok) {
